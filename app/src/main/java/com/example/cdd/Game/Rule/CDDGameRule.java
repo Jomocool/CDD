269a960a467bf61e92bf9667ba4184ffecfcdc19
Jomo <13530228968@163.com>
2023-06-11 21:40:11 +0800

    机器人玩家

--- app/src/main/java/com/example/cdd/Game/Rule/CDDGameRule.java
+++ app/src/main/java/com/example/cdd/Game/Rule/CDDGameRule.java
@@ -1,5 +1,14 @@
 package com.example.cdd.Game.Rule;
 
-public class CDDGameRule extends GameRule{
+import com.example.cdd.Game.Domain.CardGroup;
+import com.example.cdd.Game.Domain.PokerType;
 
+public class CDDGameRule extends GameRule{
+    //比较牌大小
+    public boolean judge(CardGroup playerCards, CardGroup deskCards){
+        //不是相同数量的牌组无法比较，如果牌型无效也无法比较，由于已经打出的牌deskCards肯定是有效的，所以不用做额外判断
+        return  playerCards.getType()!= PokerType.cError&&
+                playerCards.getCards().size()==deskCards.getCards().size()&&
+                playerCards.getMaxCard()>deskCards.getMaxCard();
+    }
 }
