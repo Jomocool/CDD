110f6e47315fe94f0c5d8a92ad0ea7f357dadbc3
Jomo <13530228968@163.com>
2023-06-12 21:43:35 +0800

    机器人打牌和计分

--- app/src/main/java/com/example/cdd/Game/System/GameTurn.java
+++ app/src/main/java/com/example/cdd/Game/System/GameTurn.java
@@ -21,6 +21,18 @@ public class GameTurn {
     public RobotPlayer player3=new RobotPlayer("人机3");
     public RobotPlayer player4=new RobotPlayer("人机4");
 
+    //player1得分
+    int score1;
+
+    //player2得分
+    int score2;
+
+    //player3得分
+    int score3;
+
+    //player4得分
+    int score4;
+
     //记录上一位玩家的牌
     ArrayList<Integer> LastPlayerCards=new ArrayList<>();
 
@@ -34,6 +46,10 @@ public class GameTurn {
     //构造函数，初始时为每位玩家发牌,并正常排序玩家的牌
     public GameTurn()
     {
+        score1=0;
+        score2=0;
+        score3=0;
+        score4=0;
         shuffle_cards();;
         deal_cards();
         player1.normal_sort();
@@ -71,5 +87,44 @@ public class GameTurn {
         player2.show_player_card();
         player3.show_player_card();
         player4.show_player_card();
+    }
+
+    public int getScore1() {
+        return score1;
+    }
+
+    public int getScore2() {
+        return score2;
+    }
+
+    public int getScore3() {
+        return score3;
+    }
+
+    public int getScore4() {
+        return score4;
+    }
+
+    //计算最终得分
+    public void computeScore(){
+        int cardScore1=computeCardScore(player1.getArrayList().size());
+        int cardScore2=computeCardScore(player2.getArrayList().size());
+        int cardScore3=computeCardScore(player3.getArrayList().size());
+        int cardScore4=computeCardScore(player4.getArrayList().size());
+
+        int sum=score1+score2+score3+score4;
+
+        score1=sum-4*score1;
+        score2=sum-4*score2;
+        score3=sum-4*score3;
+        score4=sum-4*score4;
+    }
+
+    //传入剩余牌数，计算牌分
+    public int computeCardScore(int n){
+        if(n<8)return n;
+        else if(n>=8&&n<10)return n*2;
+        else if(n>=10&&n<13)return n*3;
+        else return n*4;
     }
 }
